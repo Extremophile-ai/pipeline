cb912b5a17f97603274011da9d548809551e7a2c
Godspower <godspowercuche56@gmail.com>
2022-08-08 10:39:41 +0100

    - add control for valid date - dummy data for explainer url

--- src/services/util.services.ts
+++ src/services/util.services.ts
@@ -5,6 +5,14 @@ export default {
       if (dob.match(validDateFormatRegex) === null) {
         throw new Error("valid dob format is: YYYY-MM-DD")
       }
+
+      const isValiddate = new Date(dob);
+      const timestamp = isValiddate.getTime();
+    
+      if (typeof timestamp !== 'number' || Number.isNaN(timestamp)) {
+        throw new Error("please enter a valid date")
+      }
+
       return true
     } catch (error) {
       throw error
